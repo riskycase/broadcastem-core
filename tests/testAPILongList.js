a7fc45725a273e2c57f696366f7551707d1f474c
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-08-15 11:43:19 +0530

    Rewrite tests to verify JSON
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- tests/testAPILongList.js
+++ tests/testAPILongList.js
@@ -46,6 +46,9 @@ function testInvalidDownload (done, index) {
 	})
 	.end((err, res) => {
 		res.should.have.property('status',400);
+		res.body.should.be.a('object');
+		res.body.should.have.property('status', 400);
+		res.body.should.have.property('message', 'Wrong data supplied!');
 		done();
 	});
 }
@@ -117,6 +120,9 @@ describe('When sharing with repititions', () => {
 		.post('/upload')
 		.end((err, res) => {
 			res.should.have.property('status',400);
+			res.body.should.be.a('object');
+			res.body.should.have.property('status', 400);
+			res.body.should.have.property('message', 'Please choose files');
 			done();
 		});
 	});
