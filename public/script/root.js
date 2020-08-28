ae04919ec3dd08c533d7e71716e6b6610665cccc
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-08-28 11:35:42 +0530

    Remove duplicate code in public/script/root.js
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- public/script/root.js
+++ public/script/root.js
@@ -106,39 +106,10 @@ UIkit.upload('.js-upload', {
 	url: '/upload',
 	multiple: true,
 	type: 'multipart/form-data',
-	beforeSend: function () {
-		console.log('beforeSend', arguments);
-	},
-	beforeAll: function () {
-		console.log('beforeAll', arguments);
-	},
-	load: function () {
-		console.log('load', arguments);
-	},
-	error: function () {
-		console.log('error', arguments);
-	},
-	complete: function () {
-		console.log('complete', arguments);
-	},
-	loadStart: function (e) {
-		console.log('loadStart', arguments);
-		bar.removeAttribute('hidden');
-		bar.max = e.total;
-		bar.value = e.loaded;
-	},
-	progress: function (e) {
-		console.log('progress', arguments);
-		bar.max = e.total;
-		bar.value = e.loaded;
-	},
-	loadEnd: function (e) {
-		console.log('loadEnd', arguments);
-		bar.max = e.total;
-		bar.value = e.loaded;
-	},
+	loadStart: setBarValue,
+	progress: setBarValue,
+	loadEnd: setBarValue,
 	completeAll: function () {
-		console.log('completeAll', arguments);
 		setTimeout(function () {
 			bar.setAttribute('hidden', 'hidden');
 		}, 1000);
@@ -146,6 +117,15 @@ UIkit.upload('.js-upload', {
 		loadFiles();
 	},
 });
+
+/*
+ * Sets the value for the progress bar
+ */
+function setBarValue(e) {
+	bar.removeAttribute('hidden');
+	bar.max = e.total;
+	bar.value = e.loaded;
+}
 
 /*
  * Once the button for downloading the selected files is clicked, sends the
