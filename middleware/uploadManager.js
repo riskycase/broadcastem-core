d1d6dbe640851ec27059323bd50f83df304c84b7
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-10-26 12:31:01 +0530

    Remove multer dependency
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- middleware/uploadManager.js
+++ middleware/uploadManager.js
@@ -1,34 +1,11 @@
-const multer = require('multer');
 const busboy = require('busboy');
 const fs = require('fs');
 const path = require('path');
 
 const fileManager = require('./fileManager');
 
-// Sets destination for Multer to save files to, and preserve the original name
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, fileManager.destination());
-	},
-	filename: function (req, file, cb) {
-		const dest = fileManager.destination();
-		let number = 0;
-		let name;
-		do {
-			name = `${path.basename(
-				file.originalname,
-				path.extname(file.originalname)
-			)}${number-- || ''}${path.extname(file.originalname)}`;
-		} while (fs.existsSync(path.resolve(dest, name)));
-		cb(null, name);
-	},
-});
-
-// Creates an instance of Multer with the set options
-const upload = multer({ storage: storage });
-
 // Use busboy to save incoming files to disk
-module.exports.saveFiles2 = function (req, res, next) {
+module.exports.saveFiles = function (req, res, next) {
 	if (req.method !== 'POST' || !req.headers['content-type']) return next();
 	let writer = new busboy({ headers: req.headers });
 	writer.on('file', (fieldname, file, filename, encoding, mimetype) => {
@@ -61,8 +38,5 @@ module.exports.saveFiles2 = function (req, res, next) {
 	});
 	req.pipe(writer);
 };
-
-// Exposes the created Multer instance to the router
-module.exports.saveFiles = upload.array('files[]');
 
 module.exports.updateReceivedFiles = fileManager.updateReceivedFiles;
