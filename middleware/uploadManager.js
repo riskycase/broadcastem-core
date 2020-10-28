ac1368357a601a2b20d564342d90100bf1a62931
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-10-28 15:30:02 +0530

    Refactor code
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- middleware/uploadManager.js
+++ middleware/uploadManager.js
@@ -5,109 +5,128 @@ const path = require('path');
 
 const fileManager = require('./fileManager');
 
+/*
+ * Returns a unique name for the file to prevent overwriting
+ *
+ * Takes parent directory and name of file/directory, returns a resolved path
+ */
+function uniqueName(name, folder) {
+	const parent = fileManager.destination();
+	let number = 0;
+	let filename;
+	do
+		filename = `${path.basename(name, path.extname(name))}${
+			number-- || ''
+		}${folder ? '' : path.extname(name)}`;
+	while (fs.existsSync(path.resolve(parent, filename)));
+	return {
+		path: path.resolve(parent, filename),
+		originalname: name,
+		filename: filename,
+		size: -1,
+		folder: folder,
+	};
+}
+
+/*
+ * Extracts a zip stream to destination directory and returns final size
+ *
+ * Takes parent directory and zip stream, returns a promise that resolves with
+ * the size of the extracted folder
+ */
+function fileWriter(filePath, fileStream) {
+	return new Promise((resolve, reject) => {
+		fs.mkdirSync(path.dirname(filePath), {
+			recursive: true,
+		});
+		const destStream = fs.createWriteStream(filePath).on('close', () => {
+			resolve(destStream.bytesWritten);
+		});
+		fileStream.pipe(destStream);
+	});
+}
+
+/*
+ * Extracts a zip stream to destination directory and returns final size
+ *
+ * Takes parent directory and zip stream, returns a promise that resolves with
+ * the size of the extracted folder
+ */
+function extracter(destination, zipstream) {
+	return new Promise((resolve, reject) => {
+		let size = 0;
+		let entries = 0;
+		const unzipper = unzip
+			.Parse()
+			.on('error', reject)
+			.on('entry', entry => {
+				entries++;
+				if (entry.type === 'Directory')
+					fs.mkdirSync(path.resolve(destination, entry.path), {
+						recursive: true,
+					});
+				else {
+					fileWriter(
+						path.resolve(destination, entry.path),
+						entry
+					).then(bytesWritten => {
+						size += bytesWritten;
+						if (--entries === 0) resolve(size);
+					});
+				}
+			});
+		zipstream.pipe(unzipper);
+	});
+}
+
 // Use busboy to save incoming files to disk
 module.exports.saveFiles = function (req, res, next) {
-	if (req.method !== 'POST' || !req.headers['content-type']) return next();
-	let writer = new busboy({ headers: req.headers });
-	writer.on('file', (fieldname, file, filename, encoding, mimetype) => {
-		req.files = req.files || [];
-		const dest = fileManager.destination();
-		let number = 0;
-		let name;
-		do
-			name = `${path.basename(filename, path.extname(filename))}${
-				number-- || ''
-			}${path.extname(filename)}`;
-		while (fs.existsSync(path.resolve(dest, name)));
-		req.files.push({
-			path: path.resolve(dest, name),
-			originalname: filename,
-			filename: name,
-			size: -1,
-			folder: false,
+	if (req.method !== 'POST' || !req.headers['content-type']) next();
+	else {
+		let writer = new busboy({ headers: req.headers });
+		writer.on('file', (fieldname, file, filename, encoding, mimetype) => {
+			req.files = req.files || [];
+			let fileObject = uniqueName(filename, false);
+			req.files.push(fileObject);
+			let writeStream = fs.createWriteStream(fileObject.path);
+			writeStream.on('close', () => {
+				req.files.find(file => file.path === fileObject.path).size =
+					writeStream.bytesWritten;
+				if (!req.files.filter(file => file.size === -1).length) next();
+			});
+			file.pipe(writeStream);
 		});
-		let writeStream = fs.createWriteStream(path.resolve(dest, name));
-		writeStream.on('close', () => {
-			req.files.find(
-				file => file.path === path.resolve(dest, name)
-			).size = writeStream.bytesWritten;
-			if (!req.files.filter(file => file.size === -1).length) next();
+		writer.on('finish', () => {
+			if (!req.files) next();
 		});
-		file.pipe(writeStream);
-	});
-	writer.on('finish', () => {
-		if (!req.files) next();
-	});
-	req.pipe(writer);
+		req.pipe(writer);
+	}
 };
 
 // Use busboy to pipe zip files to an unzipper and save as a folder
 module.exports.saveZips = function (req, res, next) {
-	if (req.method !== 'POST' || !req.headers['content-type']) return next();
-	let writer = new busboy({ headers: req.headers });
-	writer.on('file', (fieldname, file, filename, encoding, mimetype) => {
-		if (mimetype === 'application/zip') {
-			req.files = req.files || [];
-			let destination;
-			let number = 0;
-			do
-				destination = path.resolve(
-					fileManager.destination(),
-					`${path.basename(filename, path.extname(filename))}${
-						number-- || ''
-					}`
-				);
-			while (fs.existsSync(path.resolve(destination)));
-			fs.mkdirSync(destination, { recursive: true });
-			req.files.push({
-				path: destination,
-				originalname: filename,
-				filename: path.basename(destination),
-				size: -1,
-				folder: true,
-			});
-			let size = 0;
-			let entries = 0;
-			const unzipper = unzip
-				.Parse()
-				.on('error', console.error)
-				.on('entry', entry => {
-					entries++;
-					if (entry.type === 'Directory')
-						fs.mkdirSync(path.resolve(destination, entry.path), {
-							recursive: true,
-						});
-					else {
-						fs.mkdirSync(
-							path.dirname(path.resolve(destination, entry.path)),
-							{ recursive: true }
-						);
-						const writeStream = fs
-							.createWriteStream(
-								path.resolve(destination, entry.path)
-							)
-							.on('close', () => {
-								size += writeStream.bytesWritten;
-								if (--entries === 0)
-									req.files.find(
-										file => file.path === destination
-									).size = size;
-								if (
-									!req.files.filter(file => file.size === -1)
-										.length
-								)
-									next();
-							});
-						entry.pipe(writeStream);
-					}
+	if (req.method !== 'POST' || !req.headers['content-type']) next();
+	else {
+		let writer = new busboy({ headers: req.headers });
+		writer.on('file', (fieldname, file, filename, encoding, mimetype) => {
+			if (mimetype === 'application/zip') {
+				req.files = req.files || [];
+				let folderObject = uniqueName(filename, true);
+				req.files.push(folderObject);
+				extracter(folderObject.path, file).then(size => {
+					req.files.find(
+						file => file.path === folderObject.path
+					).size = size;
+					if (!req.files.filter(file => file.size === -1).length)
+						next();
 				});
-			file.pipe(unzipper);
-		} else file.resume();
-	});
-	writer.on('finish', () => {
-		if (!req.files) next();
-	});
-	req.pipe(writer);
+			} else file.resume();
+		});
+		writer.on('finish', () => {
+			if (!req.files) next();
+		});
+		req.pipe(writer);
+	}
 };
 
 module.exports.updateReceivedFiles = fileManager.updateReceivedFiles;
--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const uploadManager = require('../middleware/uploadManager');
 
-/* Receive incoming files */
-router.all('/', uploadManager.saveFiles, (req, res, next) => {
+/*
+ * Runs appropriate action based on data received
+ *
+ * Takes request, response objects and next callback, returns callback result
+ * on error
+ */
+function uploadResponder(req, res, next) {
 	if (req.method === 'POST') {
 		// Gets information of the files that were uploaded
 		const files = req.files;
@@ -26,33 +31,12 @@ router.all('/', uploadManager.saveFiles, (req, res, next) => {
 		error.status = 400;
 		return next(error);
 	}
-});
+}
 
-/* Receive incoming files */
-router.all('/zip', uploadManager.saveZips, (req, res, next) => {
-	if (req.method === 'POST') {
-		// Gets information of the files that were uploaded
-		const files = req.files;
-		// Checks that files were actually uploaded
-		if (!files) {
-			const error = new Error('Please choose files');
-			error.status = 400;
-			return next(error);
-		}
-		// Notifies the server to add new files for sharing
-		uploadManager.updateReceivedFiles(files);
-		res.json(
-			files.map(file => ({
-				sentFileName: file.originalname,
-				savedFileName: file.filename,
-				size: file.size,
-			}))
-		);
-	} else {
-		const error = new Error('Only POST requests are allowed on this route');
-		error.status = 400;
-		return next(error);
-	}
-});
+/* Receives incoming files */
+router.all('/', uploadManager.saveFiles, uploadResponder);
+
+/* Receives incoming folders as zip and extracts them */
+router.all('/zip', uploadManager.saveZips, uploadResponder);
 
 module.exports = router;
